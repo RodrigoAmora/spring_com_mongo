3ef9dfe6ee7391e8953ef44b6018a77377e71448
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-08-04 01:02:30 -0300

    Commit do projeto

---
+++ src/main/java/br/com/rodrigoamora/springemongo/SpringComMongoApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoamora.springemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringComMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringComMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodrigoamora/springemongo/SpringComMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoamora.springemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringComMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
